0169dfe620eaad4b01851474c5d1c84a7e1e922f
scadriano <127499400+scadriano@users.noreply.github.com>
2023-03-20 14:38:56 -0300

    Versão final - reconhece e sintetiza voz

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ with sr.Microphone() as source:
         audio = r.listen(source)
         print(r.recognize_google(audio, language='pt'))
 '''
-
+'''
 # Assistente offline!
 from vosk import Model, KaldiRecognizer
 import pyaudio
@@ -36,3 +36,41 @@ while True:
         print(rec.PartialResult())
 
 print(rec.FinalResult())
+'''
+
+# Assistente offline com Reconhecedor e Síntese de Voz!
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import pyttsx3
+import json
+
+# Síntese de voz/fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+model = Model('model')
+rec = KaldiRecognizer(model, 16000)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream.start_stream()
+
+while True:
+    data = stream.read(8192)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        result =  rec.Result()
+        result = json.loads(result)
+
+        if result is not None:
+            text = result['text']
+
+            print(text)
+            speak(text)
